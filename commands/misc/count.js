6e719f929571adb83805a1b4e6dddcaf4d4f40e9
2wr <taolaolgc@gmail.com>
2024-07-13 18:19:10 +0700

    fix interface mismatch

--- commands/misc/count.js
+++ commands/misc/count.js
@@ -5,15 +5,16 @@ module.exports = {
                 .setName("count")
                 .setDescription("increase your count by 1"),
     async execute(interaction) {
-        if (interaction.client.storage.has(interaction.author.id)) {
-            let previousValue = interaction.client.storage.get(interaction.author.id)
-            interaction.client.storage.set(interaction.author.id, previousValue + 1)
+        let sender = interaction.author === undefined ? interaction.user : interaction.author
+        if (interaction.client.storage.has(sender.id)) {
+            let previousValue = interaction.client.storage.get(sender.id)
+            interaction.client.storage.set(sender.id, previousValue + 1)
         } else {
-            interaction.client.storage.set(interaction.author.id, 0)
+            interaction.client.storage.set(sender.id, 0)
         }
         
-        await interaction.reply(`\`${interaction.author.username}\`'s current count: ${interaction.client.storage.get(interaction.author.id).toString()}`)
-        if (interaction.client.storage.get(interaction.author.id) == 727) {
+        await interaction.reply(`\`${sender.username}\`'s current count: ${interaction.client.storage.get(sender.id).toString()}`)
+        if (interaction.client.storage.get(sender.id) == 727) {
             await interaction.followUp("WYSI WYFSI!!!")
         }
     },
