b2f8e2416b5ba29501bb11034652067cc238e2d8
2wr <taolaolgc@gmail.com>
2024-07-17 10:24:36 +0700

    update minigame

--- commands/misc/count.js
+++ commands/misc/count.js
@@ -43,9 +43,26 @@ module.exports = {
                 await interaction.reply("\"w!count upgrade auto\" for the next surprise")
             }
         }
+
+        if (!interaction.client.storage.get("countStorage")[sender.id].hasOwnProperty("dynamic")) {
+            if (interaction.client.storage.get("countStorage")[sender.id].count >= 272727) {
+                await interaction.reply("\"w!count upgrade dynamic\" to convert all your static into free auto\n100 static = 1 auto, you lose all static that weren't converted\n\`w!count check\` to see your current stats")
+            }
+        }
     },
     async textExecute(interaction, args) {
         if (args[1] == "upgrade") {
+            // if (args[2] == "x") {
+            //     let sender = interaction.author === undefined ? interaction.user : interaction.author
+
+            //     let countStorage = {}
+            //     if (interaction.client.storage.has("countStorage")) {
+            //         countStorage = interaction.client.storage.get("countStorage")
+            //     }
+
+            //     return
+            // }
+
             if (args[2] == "static") {
                 let sender = interaction.author === undefined ? interaction.user : interaction.author
 
@@ -148,8 +165,6 @@ module.exports = {
 
                         await interaction.reply(`bro ur broke, get ${a[0]} counts then come back`)
                         if (countStorage[sender.id].autoUpgrade >= 1.5) {
-                            
-
                             let text = `cost scaling start after getting more than +1.5 on auto upgrade`
 
                             text += `\ncurrent scaling factor: (x${a[1]}) (autoUpgrade^2 * 2)`
@@ -160,6 +175,61 @@ module.exports = {
                             
                             await interaction.reply(text)
                         }
+                    }
+                }
+
+                return
+            }
+
+            if (args[2] == "dynamic") {
+                let sender = interaction.author === undefined ? interaction.user : interaction.author
+
+                let countStorage = {}
+                if (interaction.client.storage.has("countStorage")) {
+                    countStorage = interaction.client.storage.get("countStorage")
+                }
+
+                if (countStorage[sender.id] !== undefined) {
+                    let price = 272727
+
+                    if (countStorage[sender.id].staticUpgrade > 0) {
+                        let a = [
+                            price,
+                            countStorage[sender.id].staticUpgrade,
+                            Math.floor(countStorage[sender.id].staticUpgrade / 100),
+                        ]
+                        
+                        a.forEach((value, index) => {
+                            if (value > 1e9) {
+                                a[index] = value.toExponential(2)
+                            }
+                        })
+
+                        if (countStorage[sender.id].count >= price) {
+                            countStorage[sender.id].count -= price
+                            if (countStorage[sender.id].autoUpgrade !== undefined) {
+                                countStorage[sender.id].dynamic += Math.floor(countStorage[sender.id].staticUpgrade / 100)
+                                countStorage[sender.id].autoUpgrade += Math.floor(countStorage[sender.id].staticUpgrade / 100)
+                            } else {
+                                countStorage[sender.id].dynamic = Math.floor(countStorage[sender.id].staticUpgrade / 100)
+                                countStorage[sender.id].autoUpgrade = Math.floor(countStorage[sender.id].staticUpgrade / 100)
+                            }
+
+                            countStorage[sender.id].staticUpgrade = 0
+                            
+
+                            interaction.client.storage.set("countStorage", countStorage)
+
+                            await interaction.reply(`converted ${a[1]} staticUpgrades into ${a[2]} autoUpgrades\n100 static = 1 auto`)
+                        }
+
+                        if (countStorage[sender.id].count < price) {    
+                            await interaction.reply(`bro ur broke, get ${a[0]} counts then come back`)
+                        }
+                    } 
+
+                    if (countStorage[sender.id].staticUpgrade <= 0) {
+                        await interaction.reply("are you sure you want to do this? actually how did you even get here in the first place")
                     }
                 }
 
@@ -230,6 +300,44 @@ module.exports = {
 
                 return
             }
+        }
+
+        if (args[1] == "check") {
+            let sender = interaction.author === undefined ? interaction.user : interaction.author
+
+            let countStorage = {}
+            if (interaction.client.storage.has("countStorage")) {
+                countStorage = interaction.client.storage.get("countStorage")
+            }
+
+            let text = `${sender.username}'s current status:\n`
+
+            if (countStorage[sender.id] !== undefined) {
+                let a = [
+                    countStorage[sender.id].count,
+                    countStorage[sender.id].staticUpgrade,
+                    countStorage[sender.id].autoUpgrade,
+                    countStorage[sender.id].autoStart,
+                    countStorage[sender.id].dynamic,
+                ]
+                a.forEach((value, index) => {
+                    if (value > 1e9) {
+                        a[index] = value.toExponential(2)
+                    }
+                })
+
+                if (countStorage[sender.id].hasOwnProperty("count")) { text += `count: ${a[0]}\n` }
+                if (countStorage[sender.id].hasOwnProperty("staticUpgrade")) { text += `staticUpgrade: ${a[1]}\n` }
+                if (countStorage[sender.id].hasOwnProperty("autoUpgrade")) { text += `autoUpgrade: ${a[2].toFixed(2)}\n` }
+                if (countStorage[sender.id].hasOwnProperty("autoStart")) { text += `last auto claim: <t:${countStorage[sender.id].autoStart}:R>\n` }
+                if (countStorage[sender.id].hasOwnProperty("dynamic")) { text += `dynamic: +${a[4]} autoUpgrade(s)\n` }
+            } else {
+                text += "bro haven't even played the game yet"
+            }
+
+            interaction.reply(text)
+
+            return
         }
 
         await this.execute(interaction)
